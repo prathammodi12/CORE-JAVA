5efbb33bf813f073b8234b8146ad8f00502613c7
prathammodi12 <prathammodi17@gmail.com>
2025-10-09 15:33:29 +0530

    Practicals of WrapperClasses

---
+++ src/main/java/com/pratham/corejava/WrapperClasses/PrimitiveAndString.java
@@ -0,0 +1,11 @@
+package com.pratham.corejava.WrapperClasses;
+
+public class PrimitiveAndString {
+    public static void main(String[] args) {
+        byte x= 100;
+
+        String s=Byte.toString(x);
+
+        byte y=Byte.parseByte(s);
+    }
+}
