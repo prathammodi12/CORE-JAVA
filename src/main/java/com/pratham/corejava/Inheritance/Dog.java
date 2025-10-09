008d0fdc58d4dbc0b289a6b8476869f9ce06f035
prathammodi12 <prathammodi17@gmail.com>
2025-10-09 15:30:08 +0530

    Practicals of Inheritance

---
+++ src/main/java/com/pratham/corejava/Inheritance/Dog.java
@@ -0,0 +1,9 @@
+package com.pratham.corejava.Inheritance;
+
+public class Dog extends Animal {
+
+    @Override
+    public void sayHello(){
+        System.out.println("Woof");
+    }
+}
---
+++ src/main/java/com/pratham/corejava/Inheritance/TestInheritance.java
@@ -0,0 +1,20 @@
+package com.pratham.corejava.Inheritance;
+
+public class TestInheritance {
+
+//    void m1(){
+//        System.out.println("Inside m1");
+//    }
+//
+//    public static void main(String[] args){
+//        TestInheritance s1= new TestInheritance();
+//        s1.m1();
+//        s1.hashCode();
+//        s1.getClass();
+//    }
+
+    public static void main(String[] args) {
+        Dog dog= new Dog();
+        dog.sayHello();
+    }
+}
